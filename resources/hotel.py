63551a8ec8fae11c055be92f4c514e8146f3b843
Lucas <lucas.cabral@virtus.ufcg.edu.br>
2021-04-26 20:21:58 -0300

    Adding login system

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
+from flask_jwt_extended import jwt_required
 
 
 class Hoteis(Resource):
@@ -20,6 +21,7 @@ class Hotel(Resource):
             return hotel.json(), 200
         return {'message': 'Hotel not found.'}, 404
 
+    @jwt_required()
     def post(self, hotel_id):
         if HotelModel.find_hotel(hotel_id):
             return {"message": "Hotel id '{}' already exists.".format(hotel_id)}, 400
@@ -31,6 +33,7 @@ class Hotel(Resource):
             return {'message': 'An internal error ocurred trying to save hotel'}, 500
         return hotel.json(), 200
 
+    @jwt_required()
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
         hotel_encontrado = HotelModel.find_hotel(hotel_id)
@@ -45,6 +48,7 @@ class Hotel(Resource):
             return {'message': 'An internal error ocurred trying to save hotel'}, 500
         return hotel.json(), 201
 
+    @jwt_required()
     def delete(self, hotel_id):
         hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
